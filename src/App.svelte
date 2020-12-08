b87e8d1d73ad2716d90f58d88257f7375bbc342f
Nguyen Thi Hoang Trang <trang.nguyen-hoang@internal.banvien.com>
2020-12-08 13:02:41 +0700

    basic (not UI)

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,65 @@
 <script>
-	let name = 'trang';
+  let newItem = "";
+
+  let todoList = [
+    { text: "Write my first post", status: true },
+    { text: "Upload the post to the blog", status: false },
+    { text: "Publish the post at Facebook", status: false },
+  ];
+
+  let searchTerm = "";
+
+  $: filteredList = todoList.filter(
+    (item) => item.text.indexOf(searchTerm) !== -1
+  );
+
+  const addToList = () => {
+    if (/\S+/.test(newItem)) {
+      todoList = [...todoList, { text: newItem, status: false }];
+      newItem = "";
+    }
+  };
+
+  const removeFromList = (index) => {
+    todoList.splice(index, 1);
+    todoList = todoList;
+  };
+
+  const handleKeyup = () => {
+    if (event.code == "Enter" && /\S+/.test(newItem)) {
+      todoList = [...todoList, { text: newItem, status: false }];
+      newItem = "";
+    }
+  };
+
+  const clearFilter = () => {
+    searchTerm = "";
+  };
 </script>
 
-<h1>Hello {name}!</h1>
+<style>
+  .checked {
+    text-decoration: line-through;
+  }
+</style>
+
+<h3>Filter by name</h3>
+<input bind:value={searchTerm} />
+<button on:click={clearFilter}>Clear</button>
+
+<h3>TODO</h3>
+<br />
+{#each filteredList as item, index}
+  <input bind:checked={item.status} type="checkbox" />
+  <span class:checked={item.status}>{item.text}</span>
+  <span on:click={() => removeFromList(index)}>❌</span>
+  <br />
+{/each}
+
+<h3>ADD ITEM</h3>
+<input
+  bind:value={newItem}
+  type="text"
+  placeholder="new todo item.."
+  on:keyup|preventDefault={handleKeyup} />
+<button on:click={addToList}>Add</button>
---
+++ src/components/Item.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let status;
+  export let text;
+</script>
+
+<style>
+  .checked {
+    text-decoration: line-through;
+  }
+</style>
+
+<input bind:checked={status} type="checkbox" />
+<span class:checked={status}>{text}</span>
+<button>Delete</button>
